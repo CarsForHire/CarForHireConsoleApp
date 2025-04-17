d27be9e66e28835125ad1a5a99afa851ebec9a67
Lewis Kelly <lewis.kelly747@gmail.com>
2025-04-17 15:17:52 +0100

    Alternative Program file code is commented out. This one s to initiate the forms in the other files. I think the app needs to be a windows forms app for it to all work however. Connection is set to Localhost so hopefully will work for others. DB & Table names changed to mathc sql shared by Matthew.

---
+++ Alternative_Program.cs
@@ -0,0 +1,19 @@
+/*using System;
+using System.Windows.Forms;
+
+namespace DraftCarHireApp
+{
+    static class Program
+    {
+     
+        ///  The main entry point for the application.
+       
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmCars());
+        }
+    }
+}*/
